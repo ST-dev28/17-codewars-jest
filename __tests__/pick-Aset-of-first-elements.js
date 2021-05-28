085c52302bd2a2f50ed6939c425187732d26f089
SandraT <sndrbltr@gmail.com>
2021-05-28 22:52:42 +0000

    Next batch of tests passed #2

--- __tests__/pick-Aset-of-first-elements.js
+++ __tests__/pick-Aset-of-first-elements.js
@@ -5,16 +5,24 @@ will return the first n elements of the sequence.
 If n == 0 return an empty sequence []
 */
 
-function first(arr, n) {
-    const newArr = arr.slice(0, n)
-    if (n === 0) {
-        return [];
-    }
-    return newArr;
-}
 
+
+function first(arr, n) {
+    if (n === 0) return [];
+    if(n >= 0) return n >= 0 ? arr.slice(0, n) : [];
+    return arr[0];
+}
+/*
+function first(arr, n) {
+    if (n === 0) return [];
+    if(n >= 1) return arr.slice(0, n);
+    
+    return arr.slice(0, -n);
+    }
+*/
 test("first elements of asequence", () => {
     var arr = ['a', 'b', 'c', 'd', 'e'];
     expect(first(arr)).toBe(['a']);
     expect(first(arr, 2)).toBe(['a', 'b']);
+    expect(first(arr, 3)).toBe(['a', 'b', 'c']);
 });
